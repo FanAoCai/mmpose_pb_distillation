1b46bf314969e9e6fb3ace785ee18c31819908ba
unknown <1846109672@qq.com>
2022-11-04 19:30:08 +0800

    first commit

---
+++ mmpose/core/runner/__init__.py
@@ -0,0 +1,12 @@
+'''
+Author: Peng Bo
+Date: 2022-10-14 17:02:11
+LastEditTime: 2022-10-14 17:15:45
+Description: 
+
+'''
+# Copyright (c) OpenMMLab. All rights reserved.
+from .distillation_runner import DistillationRunner
+
+__all__ = ['DistillationRunner']
+
